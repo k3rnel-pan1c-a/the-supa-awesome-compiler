aa44ee30b5d2bca195b45feb6b038773452b65ff
seifeldeen mohamed galal <seifmo@Seifmo-MacBook-Pro.local>
2024-12-21 16:42:15 +0200

    modified main.py to use compiler class from compiler.py

--- the_supa_awesome_compiler/main.py
+++ the_supa_awesome_compiler/main.py
@@ -1,15 +1,29 @@
 import json
 
-from _lexer import Lexer
-from _parser import Parser
-
+from the_supa_awesome_compiler._lexer import Lexer
+from the_supa_awesome_compiler._parser import Parser
+from the_supa_awesome_compiler.compiler import Compiler
 
 if __name__ == "__main__":
+
     with open("../tests/func.marsh", "r") as f:
         source_code = [line.rstrip() for line in f.readlines()]
 
+    
+    parser = Parser(Lexer(source_code))
+    program = parser.parse_program()
 
-parser = Parser(Lexer(source_code))
-program = parser.parse_program()
-with open("../debug/ast.json", "w") as f:
-    json.dump(program.json_repr(), f, indent=4)
+    
+    with open("../debug/ast.json", "w") as f:
+        json.dump(program.json_repr(), f, indent=4)
+
+    
+    compiler = Compiler()
+    compiler.compile(program)
+
+   
+    ir_text = str(compiler.module)
+    with open("ir.ll", "w") as f:
+        f.write(ir_text)
+
+    print("Compilation complete! IR written to ir.ll")
